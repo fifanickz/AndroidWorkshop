42bc1af7da342913f0e04914d7b7316e06d50d28
unknown <ARIT@DESKTOP-R41VNG9.ARIT.LOCAL>
2018-09-09 16:42:34 +0700

    MVVM

---
+++ app/src/main/java/com/arit/demo/mvvm/MainActivity.java
@@ -0,0 +1,19 @@
+package com.arit.demo.mvvm;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.arit.demo.mvvm.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        User user = new User("fifanickz","madsathan");
+        activityMainBinding.setUser(user);
+    }
+}
